70f260b62d2c84c98392ab5f9adbff3a4c04cc2c
Red-Fox <ester_c_rocha@estudante.sesisenai.org.br>
2024-08-05 08:04:40 -0300

    hey

--- index.js
+++ index.js
@@ -8,104 +8,83 @@ const Produto = require('./model/Produto')
 
 const PORT = 3000
 const hostname = 'localhost'
-
-//----------------- Configurações Express -----------------
-
+// ---------------------------------------
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
+// ----------------------------------------
 
-//---------------------------------------------------------
-
-
-//---------------------- Fabricante ----------------------- 
-
-app.get('/fabricante', (req, res) => {
-    const valores = req.query
-    console.log(valores)
-
-    try {
-        const pesq = await Fabricante.findOne({ where: { marca: valores.marca }, raw: true })
-        if (pesq === null) {
-            console.log(pesq)
-            res.status(200).json(pesq)
-        } else if (pesq.marca == valores.marca) {
-
-        }
-    } catch (err) {
-        console.error('Não foi possível realizar a pesquisa')
-        res.status(500).json({ message: 'Não foi possível realizar a pesquisa' })
-    }
-})
-
-//---------------------------------------------------------
-
-app.post('/fabricante', async (req, res) => {
-    const valores = req.body
-    console.log("Aplicação Rodando!")
-
-    try {
-        const pesq = await Fabricante.create(valores, { raw: true })
-        console.log(valores)
-        res.status(200).json(pesq)
-    } catch (err) {
-        console.error('Não foi possível gravar dados')
-        res.status(500).json({ message: 'Não foi possível gravar dados' })
-    }
-
-
-    res.status(200).json({ message: 'Valores Recebidos!' })
-})
-
-
-
-//------------------------ Produto ------------------------
-
+// ----------------- Post -----------------
 app.post('/produto', async (req, res) => {
     const valores = req.body
     console.log(valores)
-
-    if (valores.gabricanteId == 1) {
-        const pesq = await Produto.create(valores, { raw: true })
-        res.status(201).json(pesq)
-    } else {
-        res.status(404).json({ message: 'Fabricante não encontrado' })
-
-    }
-})
-
-
-//---------------------------------------------------------
-
-app.get('/produto', (req, res) => {
-    const valores = req.query
-    console.log(valores)
-
+    
     try {
-        const pesq = await Produto.findOne({ where: { marca: valores.nome }, raw: true })
-        if (pesq === null) {
-            console.log(pesq)
-            res.status(200).json(pesq)
-        } else if (pesq.nome == valores.nome) {
-
+        if (valores.fabricanteId == 1) {
+            const pesq = await Produto.create(valores, { raw: true })
+            res.status(201).json(pesq)
+        } else {
+            res.status(404).json({ message: "fabricante não existe" })
         }
     } catch (err) {
-        console.error('Não foi possível realizar a pesquisa')
-        res.status(500).json({ message: 'Não foi possível realizar a pesquisa' })
+        console.error('Não foi possível consultar os dados!')
+        res.status(500).json({ message: "Não foi possível consultar os dados!" })
     }
 })
 
-
-//---------------------------------------------------------
-
-conn.sync().then(() => {
-    app.listen(PORT, hostname, () => {
-        console.log(`Servidor rodando em http://${hostname}:${PORT}`)
-    })
-}).catch((err) => {
-    app.listen(PORT, hostname, () => {
-        console.error('Banco de Dados não conectado', err)
-    })
+// ----------------- Get ------------------
+app.get('/fabricante', async (req, res) => {
+    const valores = req.query
+    console.log(valores)
+    
+    try {
+        const pesq = await Fabricante.findOne({ where: { marca: valores.marca }, raw: true })
+        if (pesq === null) {
+            console.log('valores inexistentes!')
+            res.status(404).json({ message: "valores inexistentes!" })
+        } else if (pesq.marca == valores.marca) {
+            console.log(pesq.marca)
+            res.status(200).json(pesq.marca)
+        }
+    } catch (err) {
+        console.error('Não foi possível consultar os dados!')
+        res.status(500).json({ message: "Não foi possível consultar os dados!" })
+    }
+    
 })
 
-//---------------------------------------------------------
+app.post('/fabricante', async (req, res) => {
+    const valores = req.body
+    console.log(valores)
+    
+    try {
+        const pesq = await Fabricante.create(valores, { raw: true })
+        console.log(pesq)
+        res.status(201).json(pesq)
+    } catch (err) {
+        console.error('Não foi possível gravar os dados!')
+        res.status(500).json({ message: "Não foi possível gravar os dados!" })
+    }
+})
+
+// --------------- Delete -----------------
+app.delete('/fabricante/:id', (req, res) => {
+    const valor = req.params
+    console.log(valor)
+    console.log('---------------')
+    console.log(valor.id)
+    res.status(200).json({ message: "dados recebidos" })
+})
+
+app.get('/', (req, res) => {
+    console.log('aplicação rodando!')
+    res.status(200).json({ message: 'aplicação rodando!' })
+})
+// ----------------------------------------
+conn.sync().then(() => {
+    app.listen(PORT, hostname, () => {
+        console.log(`O servidor está rodando em ${hostname}:${PORT}`)
+    })
+}).catch((err) => {
+    console.error('Banco de dados não conectado!', err)
+})
